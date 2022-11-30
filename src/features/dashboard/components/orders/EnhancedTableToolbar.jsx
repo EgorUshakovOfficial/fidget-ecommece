d0812a8b6bbdc6274b38add7f9383c9bb1cc4068
Egor Ushakov <egorushakov13@gmail.com>
2022-11-30 11:25:56 -0700

    Modified tooltip for orders page

--- src/features/dashboard/components/orders/EnhancedTableToolbar.jsx
+++ src/features/dashboard/components/orders/EnhancedTableToolbar.jsx
@@ -1,22 +1,50 @@
-import {Toolbar, IconButton, Tooltip} from '@mui/material';
-import {FilterList} from '@mui/icons-material';
+import {Fragment, useContext} from 'react';
+import { alpha } from '@mui/material/styles';
+import {Toolbar, IconButton, Typography, Tooltip} from '@mui/material';
+import {Delete, FilterList} from '@mui/icons-material';
 import OrderFilter from './OrderFilter';
+import { OrderContext } from '../../context/OrderContext';
 
 export default function EnhancedTableToolbar(){
+    const {numOrdersSelected} = useContext(OrderContext);
     return (
         <Toolbar
-            sx={{
-                pl:{sm:1},
-                pr:{xs:1, sm:1},
-                justifyContent:"space-between"
-            }}
+        sx={{
+            pl: { sm: 1 },
+            pr: { xs: 1, sm: 1 },
+            justifyContent:"space-between",
+            ...(numOrdersSelected > 0 && {
+              bgcolor: (theme) =>
+                alpha(theme.palette.error.main, theme.palette.action.activatedOpacity),
+            }),
+          }}
         >
-            <OrderFilter />
-            <Tooltip title="Filter list">
-                <IconButton>
-                    <FilterList />
-                </IconButton>
-            </Tooltip>
+            {numOrdersSelected ?
+                <Fragment>
+                    <Typography
+                        sx={{ flex: '1 1 100%' }}
+                        color="inherit"
+                        variant="subtitle1"
+                        component="div"
+                    >
+                        {numOrdersSelected} orders selected
+                    </Typography>
+                    <Tooltip title="Delete">
+                        <IconButton>
+                            <Delete />
+                        </IconButton>
+                    </Tooltip>
+                </Fragment>
+                :
+                <Fragment>
+                    <OrderFilter />
+                    <Tooltip title="Filter list">
+                        <IconButton>
+                            <FilterList />
+                        </IconButton>
+                    </Tooltip>
+                </Fragment>
+            }
         </Toolbar>
     )
 }
